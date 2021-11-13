2fa4108fb5e2580a1ed3b1e050f88799b41bef5d
kristofajosh <christopherjoshua18@live.com>
2021-11-13 09:51:39 +0100

    fix: favlist did not show text on empty

--- src/app/components/molecules/favouriteList/index.tsx
+++ src/app/components/molecules/favouriteList/index.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import Typography from "../../elements/typography";
-import style from "../largestCities/largest.module.scss";
-import FrostCard from "../../elements/card";
-import { useAppDispatch, useAppSelector } from "../../../store";
-import { sortCities } from "../../../helpers/sortCities";
-import { removeFromFavourites } from "../../../store/reducers/favourites/favouriteSlice";
-import { useNavigate } from "react-router-dom";
-import { locationType } from "../../../store/reducers/types";
-import { getNoteKey } from "../../../helpers/getNoteKey";
-import { clearNotes } from "../../../store/reducers/notes/notesSlice";
+import React from 'react';
+import Typography from '../../elements/typography';
+import style from '../largestCities/largest.module.scss';
+import FrostCard from '../../elements/card';
+import { useAppDispatch, useAppSelector } from '../../../store';
+import { sortCities } from '../../../helpers/sortCities';
+import { removeFromFavourites } from '../../../store/reducers/favourites/favouriteSlice';
+import { useNavigate } from 'react-router-dom';
+import { locationType } from '../../../store/reducers/types';
+import { getNoteKey } from '../../../helpers/getNoteKey';
+import { clearNotes } from '../../../store/reducers/notes/notesSlice';
 
 const FavouriteList = () => {
   const { favList } = useAppSelector((state) => state.favourites);
@@ -18,22 +18,19 @@ const FavouriteList = () => {
   const navigate = useNavigate();
 
   const gotoCity = (state: locationType) => () => {
-    navigate("/detail", { state });
+    navigate('/detail', { state });
   };
 
   const removeFromList = (selected: locationType) => () => {
     if (selected) {
       const noteKey = getNoteKey(selected);
       if (notes[noteKey] && notes[noteKey].length) {
-        let proceedings = prompt(
-          `This location have notes attached to it\nType ${selected.location.name} to continue deletion.`,
-          ""
-        );
+        let proceedings = prompt(`This location have notes attached to it\nType ${selected.location.name} to continue deletion.`, '');
         if (proceedings === selected.location.name) {
           dispatch(removeFromFavourites({ data: selected }));
           dispatch(clearNotes({ key: noteKey }));
         } else {
-          alert("Incomplete! name did not match.");
+          alert('Incomplete! name did not match.');
         }
       } else dispatch(removeFromFavourites({ data: selected }));
     }
@@ -41,29 +38,29 @@ const FavouriteList = () => {
 
   return (
     <div>
-      <div className={"div-p-decorate"}>
+      <div className={'div-p-decorate'}>
         <Typography weight={600}>Favourite Cities</Typography>
       </div>
-      <div className={style.largest}>
-        {sortCities(favList, "a-z").map((el, i) => (
-          <FrostCard key={i} className={style.largest__card}>
-            <button
-              className={style.largest__card_remove}
-              data-testid={"remove-fav"}
-              onClick={removeFromList(el)}
-            >
-              x
-            </button>
-            <div onClick={gotoCity(el)}>
-              <Typography level={"f32"}>{el?.current.temperature}°</Typography>
-              <Typography level={"f12"} style={{ whiteSpace: "nowrap" }}>
-                {el?.current.weather_descriptions[0].split(",")[0]}
-              </Typography>
-              <Typography weight={600}>{el?.location.name}</Typography>
-            </div>
-          </FrostCard>
-        ))}
-      </div>
+      {favList.length ? (
+        <div className={style.largest}>
+          {sortCities(favList, 'a-z').map((el, i) => (
+            <FrostCard key={i} className={style.largest__card}>
+              <button className={style.largest__card_remove} data-testid={'remove-fav'} onClick={removeFromList(el)}>
+                x
+              </button>
+              <div onClick={gotoCity(el)}>
+                <Typography level={'f32'}>{el?.current.temperature}°</Typography>
+                <Typography level={'f12'} style={{ whiteSpace: 'nowrap' }}>
+                  {el?.current.weather_descriptions[0].split(',')[0]}
+                </Typography>
+                <Typography weight={600}>{el?.location.name}</Typography>
+              </div>
+            </FrostCard>
+          ))}
+        </div>
+      ) : (
+        <Typography>You have not added any favourite</Typography>
+      )}
     </div>
   );
 };
